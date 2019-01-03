e5e810eeabe18cea48b36c50d17c5aee8548d274
Vitaly Burlai <vitaly.burlai@gmail.com>
2019-01-03 15:48:03 +0000

    leetcode-word-break another try

--- leetcode-word-break/break.js
+++ leetcode-word-break/break.js
@@ -6,17 +6,36 @@
  * @return {boolean}
  */
 var wordBreak = function(s, wordDict) {
-  const startsWithAtPos = (pos, word) => s.substr(pos, word.length) === word
+  const len = s.length
+  const matches = Array.from({ length: len }, () => [])
 
-  const rec = pos => {
-    if (pos === s.length) {
-      return true
-    } else {
-      const matches = wordDict.filter(word => startsWithAtPos(pos, word))
-      return matches.findIndex(word => rec(pos + word.length)) !== -1
+  let d = Array.from(wordDict)
+  d.sort((a, b) => a.length - b.length)
+
+  const buildMatches = () => {
+    for(let i = 0; i < len; i++) {
+      let subs = {}
+      for(let j = 0; j < d.length; j++) {
+        const dlen = d[j].length
+        subs[dlen] = subs[dlen] || s.substring(i, i + dlen)
+        if(subs[dlen] === d[j]) {
+          matches[i].push(dlen)
+        }
+      }
     }
   }
 
+  const rec = (start) => {
+    if (start === len) {
+      return true
+    }
+    if ((matches[start] || []).length === 0) {
+      return false
+    }
+    return matches[start].some(l => rec(start + l))
+  }
+
+  buildMatches()
   return rec(0)
 }
 
