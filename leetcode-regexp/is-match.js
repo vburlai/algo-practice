e76f576314e0c47007e219b7dbb55e1da7f11cf4
Vitaly Burlai <vitaly.burlai@gmail.com>
2019-01-14 18:53:54 +0000

    leetcode-regexp dp solution

--- leetcode-regexp/is-match.js
+++ leetcode-regexp/is-match.js
@@ -6,50 +6,33 @@
  * @return {boolean}
  */
 var isMatch = function(str, p) {
-  if (p.length === 0) {
-    return str.length === 0
+  if (!p.length) {
+    return !str.length
   }
 
-  let parts = []
-  for (let i = 0; i < p.length; i++) {
-    if (i + 1 < p.length && p[i + 1] === '*') {
-      parts.push({ char: p[i], multi: true })
-      i++
-    } else {
-      parts.push({ char: p[i] })
+  const pp = p.split(/(?=[^*])/),
+    N = str.length + 1,
+    M = pp.length + 1
+
+  let dp = Array.from({ length: M }, () =>
+    Array.from({ length: N }, () => false)
+  )
+
+  dp[0][0] = true
+  const validChar = (pattern, pos) =>
+    pattern[0] === '.' || str[pos] === pattern[0]
+
+  for (let j = 1; j < M; j++) {
+    for (let i = 0; i < N; i++) {
+      const p1 = () => i > 0 && dp[j - 1][i - 1] && validChar(pp[j - 1], i - 1)
+      const p2 = () => dp[j - 1][i] && pp[j - 1][1] === '*'
+      const p3 = () =>
+        i > 0 && dp[j][i - 1] && pp[j - 1][1] === '*' && validChar(pp[j - 1], i - 1)
+      dp[j][i] = p1() || p2() || p3()
     }
   }
 
-  const charOk = (index, partNum) =>
-    parts[partNum].char === '.' || parts[partNum].char === str[index]
-
-  const rec = (index, partNum) => {
-    if (index === str.length && partNum === parts.length) {
-      return true
-    }
-    if (partNum === parts.length) {
-      return false
-    }
-
-    if (parts[partNum].multi) {
-      let any = false
-      let matching = true
-
-      let result = matching && rec(index, partNum + 1)
-      any = any || result
-
-      for (let i = index + 1; matching && i <= str.length; i++) {
-        matching = matching && charOk(i - 1, partNum)
-        result = matching && rec(i, partNum + 1)
-        any = any || result
-      }
-      return any
-    }
-
-    return charOk(index, partNum) && rec(index + 1, partNum + 1)
-  }
-
-  return rec(0, 0)
+  return dp[M - 1][N - 1]
 }
 
 module.exports = isMatch
